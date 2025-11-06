ad49a745c222e0365f187f789ad2aa647f99e459
Philipp Jovanovic <philipp@jovanovic.io>
2025-11-06 08:47:35 +0100

    feat: move benchmarks into separate script

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This repository provides a Rust implementation of the Cryptobazaar auction protocol and, in particular, includes the code for all of the validity proofs, the AV protocol, and the results vector computation.
 
-To replicate the microbenchmarks provided in Table 1 of the paper, follow the instructions below.
-
 ## Rust
 
 To setup Rust, please follow the [official installation instructions](https://www.rust-lang.org/tools/install).
@@ -12,34 +10,5 @@ To setup Rust, please follow the [official installation instructions](https://ww
 
 There are six different microbenchmarks in total, namely for the computation of the four validity proofs, the AV protocol, and the results vector. 
 
-To reproduce the benchmarks presented in Table 1, consider the following instructions.
+To replicate the microbenchmarks provided in Table 1 of the paper, execute `./run-benchmarks.sh`.
 
-**Benchmark 1: Validity proof $\pi_{x_i}$ (Table 1a)**
-```
-make proofxi
-```
-
-**Benchmark 2: Validity proof $\pi_{r_i}$ (Table 1a)**
-```
-make proofri
-```
-
-**Benchmark 3: Validity proof $\pi_{b_i}$ (Table 1a)**
-```
-make proofbi
-```
-
-**Benchmark 4: Validity proof $\pi_{Z_i}$ (Table 1a)**
-```
-make proofzi
-```
-
-**Benchmark 5: AV matrix $Y$ (Table 1b)**
-```
-make avmatrix
-```
-
-**Benchmark 6: Results vector $R$ (Table 1c)**
-```
-make results
-```
---
+++ run-benchmarks.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "----------------------------------------------------------"
+echo "Running benchmarks for proof \\pi_{x_i}"
+echo "----------------------------------------------------------"
+
+N=128 cargo bench --bench veceq -- --sample-size 25 --measurement-time 60
+N=1024 cargo bench --bench veceq -- --sample-size 25 --measurement-time 60
+N=8192 cargo bench --bench veceq -- --sample-size 25 --measurement-time 60
+
+echo "----------------------------------------------------------"
+echo "Running benchmarks for proof \\pi_{r_i}"
+echo "----------------------------------------------------------"
+
+N=128 cargo bench --bench nonzero -- --sample-size 25 --measurement-time 60
+N=1024 cargo bench --bench nonzero -- --sample-size 25 --measurement-time 60
+N=8192 cargo bench --bench nonzero -- --sample-size 25 --measurement-time 60
+
+echo "----------------------------------------------------------"
+echo "Running benchmarks for proof \\pi_{b_i}"
+echo "----------------------------------------------------------"
+
+N=128 cargo bench --bench lderivative -- --sample-size 25 --measurement-time 60
+N=1024 cargo bench --bench lderivative -- --sample-size 25 --measurement-time 60
+N=8192 cargo bench --bench lderivative -- --sample-size 25 --measurement-time 60
+
+echo "----------------------------------------------------------"
+echo "Running benchmarks for proof \\pi_{Z_i}"
+echo "----------------------------------------------------------"
+
+N=128 cargo bench --bench ipa -- --sample-size 25 --measurement-time 60
+N=1024 cargo bench --bench ipa -- --sample-size 25 --measurement-time 60
+N=8192 cargo bench --bench ipa -- --sample-size 25 --measurement-time 60
+
+echo "----------------------------------------------------------"
+echo "Running benchmarks for AV protocol"
+echo "----------------------------------------------------------"
+
+M=32 N=128 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=32 N=1024 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=32 N=8192 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=128 N=128 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=128 N=1024 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=128 N=8192 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 75
+M=256 N=128 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=256 N=1024 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 60
+M=256 N=8192 cargo bench --bench auctioneer_r1 -- --sample-size 25 --measurement-time 75
+
+echo "----------------------------------------------------------"
+echo "Running benchmarks for results vector"
+echo "----------------------------------------------------------"
+
+M=32 N=128 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=32 N=1024 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=32 N=8192 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=128 N=128 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=128 N=1024 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=128 N=8192 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 75
+M=256 N=128 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=256 N=1024 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 60
+M=256 N=8192 cargo bench --bench auctioneer_r2 -- --sample-size 25 --measurement-time 75
